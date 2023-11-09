0b22930faac918f3b017d3fb640fb75ccf4c6b94
KyKirma <pedrohenriquekourly@hotmail.com>
2023-11-08 21:16:22 -0300

    JOGOS FUNCIONANDO

--- Interface.py
+++ Interface.py
@@ -18,6 +18,7 @@ cursor = db.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS campeonatos (Divisao VARCHAR(3), Ano INT, Regiao VARCHAR(20), Nome VARCHAR(255), PRIMARY KEY (Divisao, Ano))")
 cursor.execute("CREATE TABLE IF NOT EXISTS times (Nome VARCHAR(255) PRIMARY KEY)")
 cursor.execute("CREATE TABLE IF NOT EXISTS TPC (CampAno INT, Divisao VARCHAR(3), Time VARCHAR(255), PRIMARY KEY (CampAno, Divisao, Time))")
+cursor.execute("CREATE TABLE IF NOT EXISTS jogos (Data VARCHAR(10), Horario VARCHAR(5), TimeCasa VARCHAR(255), TimeFora VARCHAR(255), Divisao VARCHAR(3), PRIMARY KEY (Data, Horario, TimeCasa, TimeFora, Divisao))")
 
 #Pegando os itens da data base
 CSVPath = "./DataBase"
@@ -52,21 +53,30 @@ def atualizar_camp():
         df['Ano'] = df['Date'].str.extract(r'(\d{4})')
 
         for index, row in df.iterrows():
-            #Tabela Times
+
             home_team = row['HomeTeam']
             away_team = row['AwayTeam']
+            div = row['Div']
+            regiao = row['Regiao']
+            ano = row['Ano']
+            data = row['Date']
+            hora = row['Time']
+
+            #Tabela Times
             cursor.execute("INSERT IGNORE INTO times (Nome) VALUES (%s)", (home_team,))
             cursor.execute("INSERT IGNORE INTO times (Nome) VALUES (%s)", (away_team,))
 
             #Campeonatos
-            div = row['Div']
-            regiao = row['Regiao']
-            ano = row['Ano']
             nome = "{} {} {}".format(div, regiao, ano)
             cursor.execute("INSERT IGNORE INTO campeonatos (Divisao, Ano, Regiao, Nome) VALUES (%s, %s, %s, %s)", (div, ano, regiao, nome))
 
+            #TPC
             cursor.execute("INSERT IGNORE INTO TPC (CampAno, Divisao, Time) VALUES (%s, %s, %s)", (ano, div, home_team))
             cursor.execute("INSERT IGNORE INTO TPC (CampAno, Divisao, Time) VALUES (%s, %s, %s)", (ano, div, away_team))
+
+            #Jogos
+            cursor.execute("INSERT IGNORE INTO jogos (Data, Horario, TimeCasa, TimeFora, Divisao) VALUES (%s, %s, %s, %s, %s)", (data, hora, home_team, away_team, div))
+
 
     db.commit()
 
@@ -98,6 +108,22 @@ def atualizarComboTime():
         timesLista.append(item)
     timeFun["values"] = timesLista
 
+def atualizarComboJogo():
+    cursor = db.cursor()
+    cursor.execute("SELECT Divisao FROM campeonatos WHERE Nome = %s", (campCombo.get(),))
+    camp_selecionado = cursor.fetchall()
+    cursor.execute("SELECT Data, Horario, TimeCasa, TimeFora FROM jogos WHERE Divisao = %s AND TimeCasa = %s OR Divisao = %s AND TimeFora = %s", (*camp_selecionado[0], timeFun.get(), *camp_selecionado[0], timeFun.get()))
+    jogos = list(cursor.fetchall())
+
+    # Formata os valores da lista
+    jogos = [
+        "{} x {} | {} | {}".format(jogo[2], jogo[3], jogo[0], jogo[1])
+        for jogo in jogos
+    ]
+
+    jogosFun["values"] = jogos
+
+
 
 #Inicia-se a janela principal
 root = tk.Tk()
@@ -105,7 +131,6 @@ root.geometry("900x600") #Define o tamanho inicial da janela
 root.title("Projeto IA - Protótipo") #Titulo da janela
 root.iconbitmap("./Interface/IFTM.ico") #Icon da janela
 root.minsize(700, 400) #Define o tamanho minimo da janela
-root.protocol("WM_DELETE_WINDOW", ) #!RESOLVER!!----------------------------------------------
 
 #As configurações de expansão
 root.grid_rowconfigure(0, weight = 1)
@@ -127,12 +152,12 @@ funcaoBox.grid(row = 0, column = 1, sticky="nsew", padx = 20, pady = 20)
 #parte esquerda da tela, as labels
 campLabel = tk.Label(textosLabel, text="Campeonato", padx = 3, pady = 3)
 timeLabel = tk.Label(textosLabel, text="Times", padx = 3, pady = 3)
-jogosLabel = tk.Label(textosLabel, text="Jogos", padx = 15, pady = 15)
+jogosLabel = tk.Label(textosLabel, text="Jogos", padx = 3, pady = 3)
 
 #Suas posições na janela
 campLabel.pack(padx = 5, pady = 5, expand = False, fill = "both")
 timeLabel.pack(padx = 5, pady = 5, expand = False, fill = "both")
-jogosLabel.pack(padx = 5, pady = 5, expand = True, fill = "both")
+jogosLabel.pack(padx = 5, pady = 5, expand = False, fill = "both")
 
 #parte direita da tela, as caixas funcionais
 campCombo = tk.ttk.Combobox(funcaoBox)
@@ -140,15 +165,15 @@ atualizarComboCamp()
 
 timeFun = tk.ttk.Combobox(funcaoBox)
 
-jogosFun = tk.Button(funcaoBox, text = "Protótipo")
+jogosFun = tk.ttk.Combobox(funcaoBox)
 
 campCombo.bind("<<ComboboxSelected>>", lambda event, frame=timeFun: atualizarComboTime())
-
+timeFun.bind("<<ComboboxSelected>>", lambda event, frame=jogosFun: atualizarComboJogo())
 
 #Suas posições na janela
 campCombo.pack(padx = 5, pady = 5, expand = False, fill = "both")
 timeFun.pack(padx = 5, pady = 5, expand = False, fill = "both")
-jogosFun.pack(padx = 5, pady = 5, expand = True, fill = "both")
+jogosFun.pack(padx = 5, pady = 5, expand = False, fill = "both")
 
 #====================================================
 
